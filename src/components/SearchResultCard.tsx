50bf3d7636383e8f80a133674a04bb72b0641865
WipuW <wipu9402@gmail.com>
2024-04-28 18:44:27 +0700

    adjust searchvideo card

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -1,13 +1,17 @@
-import { Card, Typography } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 
 
 export default function SearchResultCard(searchResult: {start_time:string,stop_time:string, text:string}) {
     const trunc_time = searchResult.start_time.split(' ')[1].substring(0,8);
-    const trunc_text = searchResult.text.length > 200 ? searchResult.text.substring(0,100) + "..." : searchResult.text;
+    const trunc_text = searchResult.text;
     return (
-    <Card variant="outlined" style={{ marginTop:'5px' }} sx={{ "&:hover": { backgroundColor: 'lightgray' } }}>
-        <Typography variant="h6" color="dodgerblue">{trunc_time}</Typography>
-        <Typography variant="body1">{trunc_text}</Typography>
+    <Card variant="outlined"
+     style={{ marginTop:'5px', padding:'5px', cursor:'pointer'}}
+      sx={{ "&:hover": { backgroundColor: 'lightgray' } }}>
+        <Typography display='inline' variant="body2" color="dodgerblue">{trunc_time} </Typography>
+        <Typography display='inline' variant="body2">
+            {trunc_text}
+        </Typography>
     </Card>
     )
 }
